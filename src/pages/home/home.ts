3f4fd9ad0b1e12298e6f385d1ccbd36ede8ca8f0
mesut.cakir <mesut.cakir@hotmail.com.tr>
2018-04-14 00:48:24 +0300

    geolocation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  test:string="veri yok";
+  constructor(public navCtrl: NavController,private geolocation: Geolocation) {
+	this.test="Started";
+ 		console.log(this.test)
+  this.geolocation.getCurrentPosition().then((x) => {
+		this.test=JSON.stringify(x.coords);
+ 		console.log(this.test)
+}).catch((error) => {
+  console.log('Error getting location', error);
+});
 
   }
+
 
 }
