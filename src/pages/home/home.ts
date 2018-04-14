35722961d666ec6ea2ff11cd13838d288792663f
mesut.cakir <mesut.cakir@hotmail.com.tr>
2018-04-14 13:05:14 +0300

    bug_fix

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,41 +4,42 @@ import { Geolocation } from '@ionic-native/geolocation';
 import { Storage } from '@ionic/storage';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+selector: 'page-home',
+templateUrl: 'home.html'
 })
 export class HomePage {
-  lat: number = 0;
-  lng: number = 0;
-  test: any = [];
-  add(typ: string, str: string) {
-    let dt = new Date();
-    this.test.push(dt.toISOString() + " | " + typ + ':' + str);
-    this.storage.set('test', JSON.stringify(this.test));
-  };
-  constructor(public navCtrl: NavController, private geolocation: Geolocation
-   , private storage: Storage) {
-		this.cation.getCurrentPosition().then((resp) => {
-		 this.add('current', resp.coords.longitude + ' ' + resp.coords.latitude);
-				this.lat=resp.coords.latitude;
-				this.lng=resp.coords.longitude;
-		}).catch((error) => {
-		  console.log('Error getting location', error);
-		})
+lat: number = 0;
+lng: number = 0;
+test: any = [];
+add(typ: string, str: string) {
+let dt = new Date();
+this.test.push(dt.toISOString() + " | " + typ + ':' + str);
+this.storage.set('test', JSON.stringify(this.test));
+};
+constructor(public navCtrl: NavController,public geolocation: Geolocation,
+public storage: Storage) { }
+ionViewDidLoad() {
+let watch = this.geolocation.watchPosition();
+this.geolocation.getCurrentPosition().then((resp) => {
+if (resp && resp.coords){
+this.add('current', resp.coords.longitude + ' ' + resp.coords.latitude);
+this.lat=resp.coords.latitude;
+this.lng=resp.coords.longitude;
+console.log(resp.coords)
 
-    let watch = this.geolocation.watchPosition();
-    watch.subscribe((position) => {
-      if (position) {
-      	this.lat=position.coords.latitude;
-		this.lng=position.coords.longitude;
-        this.add('watch', position.coords.longitude + ' ' + position.coords.latitude);
-          //.then(
-            //success => console.log('Launched navigator'),
-            //error => console.log('Error launching navigator', error)
-          //);
-      } else {
-        this.add("info", "Konum Servisi Kapalı olabilir.");
-      }
-    });
-  }
+watch.subscribe((position) => {
+if (position && position.coords){
+this.lat=position.coords.latitude;
+this.lng=position.coords.longitude;
+this.add('watch', position.coords.longitude + ' ' + position.coords.latitude);        
+} else {
+this.add("info", "Konum Servisi Kapalı olabilir.");
+}
+});
+}
+
+}).catch((error) => {
+console.log('Error getting location', error);
+})
+}
 }
