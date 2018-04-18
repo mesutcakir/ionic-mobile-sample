ab1fdbdd7c3d69089858e347a73b075526f1b579
mesut.cakir <mesut.cakir@hotmail.com.tr>
2018-04-18 23:59:13 +0300

    plugin init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,8 +10,8 @@ export class HomePage {
   bannerConfig: AdMobFreeBannerConfig = {
     isTesting: true,
     autoShow: true,
-    bannerAtTop:true,
-    //id: "ca-app-pub-7691889669897119/7510818119"
+    bannerAtTop: true,
+    id: "ca-app-pub-7691889669897119/7510818119"
   };
   constructor(public admobFree: AdMobFree) {
     this.admobFree.banner.config(this.bannerConfig);
